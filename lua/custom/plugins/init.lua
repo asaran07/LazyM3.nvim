e89d111ec4782d74592358c60418721b540494cf
Arshdeep Saran <123274028+asaran07@users.noreply.github.com>
2024-06-02 10:09:33 -0700

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,3 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
+-- Add more plugins here or in other files in this directory, more info in the Kickstart readme.
+-- I still need to learn how requiring works in Lua before I can make use of this.
 return {}
